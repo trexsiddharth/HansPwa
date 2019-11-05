414a986715c2b24f5068d2eb35a9db0b4f90fd2c
Siddharth <orksiddharth@gmail.com>
2019-11-05 18:03:20 +0530

    minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'enabled', useHash: true
+    scrollPositionRestoration: 'enabled',
   })],
   exports: [RouterModule]
 })
