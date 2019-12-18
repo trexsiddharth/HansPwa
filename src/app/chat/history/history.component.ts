4854e3e459b5f662c20e22cbddc2d68c80b0260d
Siddharth <orksiddharth@gmail.com>
2019-12-18 18:01:46 +0530

    minor

--- src/app/chat/history/history.component.ts
+++ src/app/chat/history/history.component.ts
@@ -21,15 +21,17 @@ export class HistoryComponent implements OnInit, AfterViewInit, AfterViewChecked
   ngAfterViewInit(): void {
   }
   ngAfterViewChecked(): void {
-    if (document.querySelector<HTMLElement>('.mat-tab-label-active')) {
-      document.querySelector<HTMLElement>('.mat-tab-label-active' && '#mat-tab-label-0-0').style.background = 'green';
-    }
-    if (document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-1')) {
-      document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-1').style.background = '#FFD700';
-    }
-    if (document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-2')) {
-      document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-2').style.background = 'red';
-    }
+    setTimeout(() => {
+      if (document.querySelector<HTMLElement>('.mat-tab-label-active')) {
+        document.querySelector<HTMLElement>('.mat-tab-label-active' && '#mat-tab-label-0-0').style.background = 'green';
+      }
+      if (document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-1')) {
+        document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-1').style.background = '#FFD700';
+      }
+      if (document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-2')) {
+        document.querySelector<HTMLElement>('.mat-tab-label' && '#mat-tab-label-0-2').style.background = 'red';
+      }
+    }, 1000);
   }
   shareData(id: any, contacted: any) {
     const profileData = new FormData();
