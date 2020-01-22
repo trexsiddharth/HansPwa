a560b88c9107ba8781f548c69e34ffb39807ecdc
Siddharth <orksiddharth@gmail.com>
2020-01-22 23:39:31 +0530

    mninor

--- src/app/custom-checkout/custom-checkout.component.ts
+++ src/app/custom-checkout/custom-checkout.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { SubscriptionserviceService } from '../subscriptionservice.service';
+import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from 'ngx-spinner';
+import {
+  NgxNotificationService
+} from 'ngx-kc-notification';
 
 @Component({
   selector: 'app-custom-checkout',
@@ -8,12 +14,50 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 })
 export class CustomCheckoutComponent implements OnInit {
   checkOutForm = new FormGroup({
-    name: new FormControl(null, {validators: [Validators.required]})
+    name: new FormControl(null, {validators: [Validators.required]}),
+    email: new FormControl(null, {validators: [Validators.required]})
   });
-  constructor() { }
+  constructor(private subscriptionService: SubscriptionserviceService, private http: HttpClient,
+              private spinner: NgxSpinnerService, private ngxNotificationService: NgxNotificationService, ) { }
 
   ngOnInit() {
 
+  }
+
+  createSubscription(payMode) {
+    const formData = new FormData();
+    formData.append('id', localStorage.getItem('id'));
+    formData.append('mobile', localStorage.getItem('mobile_number'));
+    if (payMode === 'card') {
+    formData.append('payment_mode', '0');
+    } else {
+      formData.append('payment_mode', '1');
+    }
+    this.http.post('https://partner.hansmatrimony.com/api/createSubscription', formData).subscribe(
+      (res: any) => {
+        if (res) {
+          console.log(res);
+          if (res.status === 1) {
+            if (res.customer_id && res.customer_id !== '' && res.order_id && res.order_id !== '') {
+                this.customClickButton( payMode,res.order_id,res.customer_id);
+            }
+          } else {
+                this.ngxNotificationService.error('Something went wrong. Try again later');
+          }
+            } else {
+              this.ngxNotificationService.error('Something went wrong. Try again later');
+            }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  customClickButton( payMode,orderId,cust_id) {
+      if (this.checkOutForm.invalid) { return; }
+      // tslint:disable-next-line: max-line-length
+      this.subscriptionService.payNowCustom(3100, 'test', 0, this.checkOutForm.value.name, this.checkOutForm.value.email, localStorage.getItem('mobile_number'),payMode ,orderId,cust_id);
   }
 
 }
--- src/app/subscriptionservice.service.ts
+++ src/app/subscriptionservice.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 declare var Razorpay: any;
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionserviceService {
-
-  constructor() { }
+  constructor( private http: HttpClient ) {
+   }
   payNowT(amt, type, plan, name, email, phone) {
     let notes = {service: ''};
     let keyId;
@@ -55,5 +56,74 @@ export class SubscriptionserviceService {
   };
   const rzp = new Razorpay(options);
     rzp.open();
+}
+payNowCustom(amt, type, plan, name, email, phone,mode,orderId,cust_id) {
+  let notes = {service: ''};
+  let keyId;
+  if (amt === 3100) {
+    notes.service = 'Limited';
+  } else
+  if (amt === 5100) {
+    notes.service = 'Supreme';
+  } else
+  if (amt === 81000) {
+    notes.service = 'Premium';
+  }
+  if (type === 'test') {
+    keyId = 'rzp_test_37I1EC7DpM6Dqq';
+    // key secret = WoNRYehcPCDC5s4w6J8MUjqn;
+  } else
+  if (type === 'live') {
+    keyId = 'rzp_live_e6JpOKoIUEouZT';
+  }
+  const key = keyId;
+
+  let options = {
+    'key': key,
+    'amount': amt * 100,
+    "currency": "INR",
+    'name': ' Hans Matrimony',
+    'description': 'Order #' + orderId,
+    'customer_id': cust_id,
+    'order_id': orderId,
+    'recurring': 1,
+
+    'handler': function(response) {
+        console.log(response);
+        if (plan === 0) {
+          alert('Payment Successfull \n' + 'Your Payment ID: ' + response.razorpay_payment_id);
+          this.authorizeFirstPayment(response);
+        } else {
+          alert('Payment Successfull \n' + ' We will get back to you shortly \n' + 'Your Payment ID: ' + response.razorpay_payment_id);
+        }
+
+       },
+    'prefill': {
+        'name':  name,
+        'email': email,
+        'contact': phone,
+        'method': mode,
+
+    },
+    'notes': notes,
+    'theme': {
+        'color': 'blue'
+    }
+};
+const rzp = new Razorpay(options);
+  rzp.open();
+}
+authorizeFirstPayment(response){
+  let formData = new FormData();
+  formData.append('id', localStorage.getItem('id'));
+  formData.append('payment_id', response.razorpay_payment_id);
+  this.http.post('http://partner.hansmatrimony.com/api/authorizeFirstPayment', formData).subscribe(
+    data => {
+        console.log(data);
+    },
+    err => {
+      console.log(err);
+    }
+  );
 }
 }
