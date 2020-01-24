e1ea15e7860681b44e4421c2c9f9266396c7d473
Siddharth <orksiddharth@gmail.com>
2020-01-24 20:51:26 +0530

    minor

--- src/app/compatibility-form/compatibility-form.component.ts
+++ src/app/compatibility-form/compatibility-form.component.ts
@@ -95,7 +95,13 @@ export class CompatibilityFormComponent implements OnInit {
   date: string[] = ['1', '2' , '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
   // tslint:disable-next-line: max-line-length
   month: string[] = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  years: string[] = [];
+  years: string[] = [
+  '1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980',
+  '1981','1982','1983','1984','1985','1986','1987','1988','1989','1990',
+  '1991','1992','1993','1994','1995','1996','1997','1998','1999','2000',
+  '2001','2002','2003','2004','2005','2006','2007','2008','2009','2010',
+  '2011','2012','2013','2014','2015','2016','2017','2018','2019','2020'
+]
   errors: string[] = [];
   authMobileNumberStatus = false;
 
@@ -122,11 +128,6 @@ export class CompatibilityFormComponent implements OnInit {
   ngOnInit() {
     this.numberCheck = localStorage.getItem('RegisterNumber').substr(3, localStorage.getItem('RegisterNumber').length);
     console.log(localStorage.getItem('RegisterNumber').substr(3, localStorage.getItem('RegisterNumber').length));
-    const yrs = new Date().getFullYear();
-    const min = yrs - 80;
-    for (let index = min; index < yrs; index++) {
-        this.years.push(index.toString());
-      }
     if (this.router.url.match('singleRegistration')) {
         this.authMobileNumberStatus = true;
       }
