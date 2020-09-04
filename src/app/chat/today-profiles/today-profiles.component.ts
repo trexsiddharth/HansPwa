a5799f1cc6476c10512b1462d68b20f3b534062b
Siddharth <orksiddharth@gmail.com>
2020-09-04 20:57:30 +0530

    minor

--- src/app/chat/today-profiles/today-profiles.component.ts
+++ src/app/chat/today-profiles/today-profiles.component.ts
@@ -645,6 +645,25 @@ export class TodayProfilesComponent implements OnInit, AfterViewInit, OnDestroy 
             this.analyticsEvent('First Profile Shown To Newly Registered');
           } else if (this.router.url.match('first')) {
             this.analyticsEvent(`Response ${reply} to first profile shown to newly registered`);
+
+            (window as any).fbq('track', 'FourPageRegistration', {
+              value: localStorage.getItem('id'),
+              content_name: localStorage.getItem('RegisterNumber'),
+            });
+            (window as any).fbq('track', '692972151223870', 'FourPageRegistration', {
+              value: localStorage.getItem('id'),
+              content_name: localStorage.getItem('RegisterNumber'),
+            });
+        
+            (window as any).fbq('track', 'CompleteRegistration', {
+              value: localStorage.getItem('id'),
+              content_name: localStorage.getItem('RegisterNumber'),
+            });
+            (window as any).fbq('track', '692972151223870', 'CompleteRegistration', {
+              value: localStorage.getItem('id'),
+              content_name: localStorage.getItem('RegisterNumber'),
+            });
+            
             this.router.navigateByUrl('chat');
           }
 
